2408f3134bc04f929fa09e9d3303307bd6e65b15
Rosaka12 <92648981+Rosaka12@users.noreply.github.com>
2023-07-24 18:29:56 +0300

    initial commit

---
+++ email.js
@@ -0,0 +1,20 @@
+let check = document.querySelector(".check");
+let password = document.querySelector(".password");
+let text = document.querySelector(".text");
+
+let regex = /^[^]+\@[a-zA-Z]+\.[a-zA-Z]{2,4}$/;
+
+check.addEventListener("click",()=>{
+	if (password.value == "") {
+		text.innerText = "Enter an Email";
+		text.style.color = "#000";
+	}
+	else if (password.value.match(regex)) {
+		text.innerText = "Yes! You entered A Valid Email Address";
+		text.style.color = "rgba(4,125,9,1)";
+	}
+	else {
+		text.innerText = "Sorry! Your Email Address is Invalid";
+		text.style.color = "rgba(255,0,0,1)";
+	}
+});
